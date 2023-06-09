c9386b8510dbea11177aaf2a3e0d0a6dc90d5871
Aviral Dewan <aviral.dewan@audacix.com>
2023-06-09 13:23:51 +0530

    made images clickable and expand on click

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,38 +1,85 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Modal } from 'react-native';
 import Swiper from 'react-native-swiper';
 
-export default function ImageCarousel({post}) {
+export default function ImageCarousel({ post }) {
+  const [selectedImage, setSelectedImage] = useState(null);
 
-    return (
-        <View style={styles.carouselContainer}>
-            <Swiper showsButtons={true} loop={false}>
-            {post.image.map((image, index) => (
-                <View style={styles.imageContainer} key={index}>
-                <Image source={{ uri: image }} style={styles.image} resizeMode="contain" />
-                </View>
-            ))}
-            </Swiper>
+  const handleImagePress = (image) => {
+    setSelectedImage(image);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
+  return (
+    <View style={styles.carouselContainer}>
+      <Swiper showsButtons={true} loop={false}>
+        {post.image.map((image, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.imageContainer}
+            onPress={() => handleImagePress(image)}
+          >
+            <Image source={{ uri: image }} style={styles.image} resizeMode="cover" />
+          </TouchableOpacity>
+        ))}
+      </Swiper>
+
+      <Modal visible={selectedImage !== null} transparent={true}>
+        <View style={styles.modalContainer}>
+          <TouchableOpacity style={styles.closeButton} onPress={handleCloseModal}>
+            <Text style={styles.closeButtonText}>Close</Text>
+          </TouchableOpacity>
+          <Image source={{ uri: selectedImage }} style={styles.expandedImage} resizeMode="contain" />
         </View>
-    );
-
+      </Modal>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    carouselContainer: {
-        marginTop: 5,
-        height: 200,
-      },
-      imageContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      image: {
-        width: '70%',
-        height: '100%',
-        borderRadius: 10,
-        borderColor: 'dodgerblue',
-        borderWidth: 1,
-      },
+  carouselContainer: {
+    marginTop: 5,
+    height: 200,
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '50%',
+    height: '100%',
+    borderRadius: 10,
+    borderColor: 'dodgerblue',
+    borderWidth: 1,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: 'dodgerblue',
+  },
+  closeButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  expandedImage: {
+    width: '80%',
+    height: '80%',
+    borderRadius: 10,
+    borderColor: 'dodgerblue',
+    borderWidth: 1,
+  },
 });
