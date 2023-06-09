d722ad051a070eeb04639eefcee8f69bb151abe3
Aviral Dewan <aviral.dewan@audacix.com>
2023-06-09 07:29:22 +0530

    added add comment icon

--- src/components/PostComments.js
+++ src/components/PostComments.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
-import { FlatList, View, Text, StyleSheet, useWindowDimensions } from 'react-native';
+import { FlatList, View, Text, StyleSheet, useWindowDimensions, TouchableOpacity } from 'react-native';
 import Comment from './Comment';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function PostComments({ comments }) {
   const renderComment = ({ item }) => {
     return <Comment comment={item} />;
   };
 
+  const createComment = () => {
+    return;
+  }
+
   const window = useWindowDimensions();
   const commentSectionHeight = window.height - 250; // Adjust the value as per your requirement
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Comments</Text>
+      <View style={{ justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
+        <Text style={styles.heading}>Comments</Text>
+        <TouchableOpacity style={{ marginRight: 10 }} onPress={createComment}>
+          <View style={styles.addComment}>
+            <Ionicons name="add" size={24} color="dodgerblue" />
+          </View>
+        </TouchableOpacity>
+      </View>
       <View style={[styles.comments, { maxHeight: commentSectionHeight }]}>
         {comments.length === 0 ? (
           <Text style={styles.noComments}>Be the first to comment!</Text>
@@ -31,12 +43,12 @@ export default function PostComments({ comments }) {
 const styles = StyleSheet.create({
   container: {
     padding: 10,
-    marginLeft: 20,
+    marginLeft: 10,
   },
   heading: {
     fontWeight: 'bold',
     fontSize: 25,
-    marginBottom: 20,
+    marginBottom: 30,
   },
   noComments: {
     fontSize: 18,
@@ -44,6 +56,15 @@ const styles = StyleSheet.create({
   },
   comments: {
     marginLeft: 10,
-    // marginBottom: 100, // Adjust the value to give extra padding at the bottom
+  },
+  addComment: {
+    backgroundColor: 'white',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: 'dodgerblue',
   },
 });
